f98e50c555767679a8adb732cedd9cba7e46ab5a
Ashraf Omar <ao356@live.com>
2023-07-12 23:47:17 +0800

    changed page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <span className="text-primary">MakerFlag</span>
           </h1>
           <p className="mt-5 text-center text-lg text-white opacity-90">
-            Technology partner for business
+            Technology partner for businesses
           </p>
           <button className="relative inline-flex items-center justify-center p-0.5 my-5 mr-2 overflow-hidden text-xl font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-green-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
